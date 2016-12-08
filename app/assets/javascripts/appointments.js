75097610d8f5dba4575e54075e87f22ff9e7845a
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-08 09:46:34 -0500

    Added reload and auto refresh functionality to welcome page

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -8,27 +8,38 @@
 //   console.log(data);
 // });
 
-$.ajax("/appointments/current", {
-  contentType: "application/json", //what we send to server
-  dataType: "json" //what we want BACK from server
-}).done(function appointmentsLoaded(data) {
-  data.forEach(function getPet(appointment) {
-    $.ajax("/pets/" + appointment.pet_id, {
-      contentType: "application/json", //what we send to server
-      dataType: "json" //what we want BACK from server
-    }).done(function (pet) {
-      console.log(pet, appointment);
-      appointment.pet = pet;
-      renderAppointment(appointment);
+function loadAppointments() {
+  $.ajax("/appointments/current", {
+    contentType: "application/json", //what we send to server
+    dataType: "json" //what we want BACK from server
+  }).done(function appointmentsLoaded(data) {
+    $(".active-appointments").html(""); // replaces everything in div with nothing, so we append the new data without duplicated what was already there
+    data.forEach(function getPet(appointment) {
+      $.ajax("/pets/" + appointment.pet_id, {
+        contentType: "application/json", //what we send to server
+        dataType: "json" //what we want BACK from server
+      }).done(function (pet) {
+        console.log(pet, appointment);
+        appointment.pet = pet;
+        renderAppointment(appointment);
+      });
     });
   });
+}
+
+$("#reload-appointments").on("click", function reloadAppointmentsHandler() {
+  window.setInterval(loadAppointments, 5 * 1000);
+  loadAppointments();
 });
+
+// this is the same thing:
+// $("#reload-appointments").on("click", loadAppointments);
 
 
 function renderAppointment (appointment) {
   $(".active-appointments").append(
     "<p>" +
-    appointment.pet.name +
+    appointment.pet.name + ": " +
     appointment.reason +
     "</p>"
   );
